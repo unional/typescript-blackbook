f681af4e7c335ab17ba2845a3db3ac0e640e7717
Homa Wong <homawong@gmail.com>
2016-03-06 17:11:12 -0800

    Add tslint test to tslint-config-unional

---
+++ packages/tslint-config-unional/spec/align.spec.ts
@@ -0,0 +1,22 @@
+
+function foo(
+  param1,
+     param2,
+        param3, param4) {
+  let a = 1;
+  a += param1 +
+  param2
+    + param3
+      + param4;
+  return a;
+ }
+
+foo(0,
+ 1,
+   2,
+     3);
+
+{
+  foo(1, 2, 3, 4);
+  foo(1, 1, 1, 1);
+}
