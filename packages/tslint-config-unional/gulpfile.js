f556ab254b4c01a558640596e47d29cdf063ac9d
Homa Wong <homawong@gmail.com>
2016-03-07 00:21:54 -0800

    add tslint-negative. Need some help

--- packages/tslint-config-unional/gulpfile.js
+++ packages/tslint-config-unional/gulpfile.js
@@ -1,12 +1,25 @@
 var gulp = require('gulp');
 var tslint = require('gulp-tslint');
+var through = require('through2');
 
-gulp.task('tslint-positive', () => {
+gulp.task('tslint-positive', function () {
   return gulp.src('spec/*.pass.ts')
     .pipe(tslint({
       rulesDirectory: "node_modules/tslint-eslint-rules/dist/rules"
     }))
     .pipe(tslint.report('verbose'));
+});
+
+gulp.task('tslint-negative', function () {
+  return gulp.src('spec/*.fail.ts')
+    .pipe(tslint({
+      rulesDirectory: "node_modules/tslint-eslint-rules/dist/rules"
+    }))
+    .pipe(through.obj(function(chunk, encode, callback) {
+      // Need some help here.
+      console.log(chunk);
+      callback();
+    }));
 });
 
 gulp.task('tslint', ['tslint-positive']);
