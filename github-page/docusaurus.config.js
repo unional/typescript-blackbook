3008316e02bb759be3d5cbf3c20536e1fe6f1924
Homa Wong <homawong@gmail.com>
2022-06-05 19:33:02 -0700

    fix: link to @antongolub
    Try enable editing

--- github-page/docusaurus.config.js
+++ github-page/docusaurus.config.js
@@ -42,7 +42,7 @@ const config = {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          // editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/unional/typescript-guidelines/tree/main/github-page/blog',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
