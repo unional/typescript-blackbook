09aa8e2b3757164b7e868d5300de57c599acf87c
unional <homawong@gmail.com>
2022-12-19 00:10:26 -0800

    feat: add github badge

--- page/src/components/Header.tsx
+++ page/src/components/Header.tsx
@@ -1,14 +1,30 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { GitHubBadge } from './GitHubBadge'
+
+const queryClient = new QueryClient()
+
 export function Header() {
   return (
-    <header>
-      <div className="flex py-5 px-3">
-        <a aria-label="home" href="/" className="hover:outline rounded-sm px-2 py-1 outline-sky-700">
-          <div className="flex">
-            <img src="/svgs/ts-guidelines.svg" className="w-6 pr-1" alt="icon" />
-            Guidelines
+    <QueryClientProvider client={queryClient}>
+      <header>
+        <nav className="flex gap-1 p-2 items-center">
+          <div className="flex pr-4">
+            <a aria-label="home" href="/" className="hover:outline rounded-sm py-1 px-2 outline-sky-700">
+              <div className="flex">
+                <div className="pr-1">
+                  <img src="/svgs/ts-guidelines.svg" className="w-6" alt="icon" />
+                </div>
+                Guidelines
+              </div>
+            </a>
           </div>
-        </a>
-      </div>
-    </header>
+          <a className="rounded-sm hover:bg-slate-300 py-1 px-2">Blog</a>
+          <a className="rounded-sm hover:bg-slate-300 py-1 px-2">Guidelines</a>
+          <div className="flex py-1 flex-grow justify-end">
+            <GitHubBadge />
+          </div>
+        </nav>
+      </header>
+    </QueryClientProvider>
   )
 }
