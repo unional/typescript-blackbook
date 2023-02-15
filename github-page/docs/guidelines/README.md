24013eb18adef7ed0f792ce4420c7e326be8c764
Homa Wong <homawong@gmail.com>
2023-02-14 17:50:40 -0800

    feat: update naming conventions

---
+++ github-page/docs/guidelines/README.md
@@ -0,0 +1,5 @@
+# Guidelines
+
+This section contains the guidelines - what you should and should not do.
+
+We will focus on HOW, while still give you sufficient reasoning on the WHY.
--- github-page/docusaurus.config.js
+++ github-page/docusaurus.config.js
@@ -6,121 +6,116 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'TypeScript Blackbook',
-  tagline: 'How to use TypeScript efficiently',
-  url: 'http://unional.github.io',
-  baseUrl: '/typescript-blackbook/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+	title: 'TypeScript Blackbook',
+	tagline: 'How to use TypeScript efficiently',
+	url: 'http://unional.github.io',
+	baseUrl: '/typescript-blackbook/',
+	onBrokenLinks: 'throw',
+	onBrokenMarkdownLinks: 'warn',
+	favicon: 'img/favicon.ico',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'unional', // Usually your GitHub org/user name.
-  projectName: 'typescript-blackbook', // Usually your repo name.
+	// GitHub pages deployment config.
+	// If you aren't using GitHub pages, you don't need these.
+	organizationName: 'unional', // Usually your GitHub org/user name.
+	projectName: 'typescript-blackbook', // Usually your repo name.
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+	// Even if you don't use internalization, you can use this field to set useful
+	// metadata like html lang. For example, if your site is Chinese, you may want
+	// to replace "en" with "zh-Hans".
+	i18n: {
+		defaultLocale: 'en',
+		locales: ['en'],
+	},
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          // editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/unional/typescript-guidelines/tree/main/github-page',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
-    ],
-  ],
+	presets: [
+		[
+			'classic',
+			/** @type {import('@docusaurus/preset-classic').Options} */
+			({
+				docs: {
+					sidebarPath: require.resolve('./sidebars.js'),
+					editUrl: 'https://github.com/unional/typescript-guidelines/tree/main/github-page',
+				},
+				blog: {
+					showReadingTime: true,
+					editUrl: 'https://github.com/unional/typescript-guidelines/tree/main/github-page',
+				},
+				theme: {
+					customCss: require.resolve('./src/css/custom.css'),
+				},
+			}),
+		],
+	],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'TypeScript Blackbook',
-        logo: {
-          alt: 'TypeScript Blackbook Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          { to: '/blog', label: 'Blog', position: 'left' },
-          {
-            type: 'doc',
-            docId: 'README',
-            position: 'left',
-            label: 'Blackbook',
-          },
-          {
-            href: 'https://github.com/unional/typescript-blackbook',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Blackbook',
-                to: '/docs/',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Discord',
-                href: 'https://discord.gg/RwzcFpN5fv',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/unional',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/unional/typescript-blackbook',
-              },
-            ],
-          },
-        ],
-       copyright: `Copyright © ${new Date().getFullYear()} by unional`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+	themeConfig:
+		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+		({
+			navbar: {
+				title: 'TypeScript Blackbook',
+				logo: {
+					alt: 'TypeScript Blackbook Logo',
+					src: 'img/logo.svg',
+				},
+				items: [
+					{ to: '/blog', label: 'Blog', position: 'left' },
+					{
+						label: 'Blackbook',
+						position: 'left',
+						to: '/docs',
+					},
+					{
+						href: 'https://github.com/unional/typescript-blackbook',
+						label: 'GitHub',
+						position: 'right',
+					},
+				],
+			},
+			footer: {
+				style: 'dark',
+				links: [
+					{
+						title: 'Docs',
+						items: [
+							{
+								label: 'Blackbook',
+								to: '/docs',
+							},
+						],
+					},
+					{
+						title: 'Community',
+						items: [
+							{
+								label: 'Discord',
+								href: 'https://discord.gg/RwzcFpN5fv',
+							},
+							{
+								label: 'Twitter',
+								href: 'https://twitter.com/unional',
+							},
+						],
+					},
+					{
+						title: 'More',
+						items: [
+							{
+								label: 'Blog',
+								to: '/blog',
+							},
+							{
+								label: 'GitHub',
+								href: 'https://github.com/unional/typescript-blackbook',
+							},
+						],
+					},
+				],
+				copyright: `Copyright © ${new Date().getFullYear()} by unional`,
+			},
+			prism: {
+				theme: lightCodeTheme,
+				darkTheme: darkCodeTheme,
+			},
+		}),
 };
 
 module.exports = config;
