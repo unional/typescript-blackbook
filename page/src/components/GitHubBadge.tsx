d8663efbbf333df49132a1020c848504ea21257b
unional <homawong@gmail.com>
2023-05-21 11:41:38 -0700

    chore: update astro

--- page/src/components/GitHubBadge.tsx
+++ page/src/components/GitHubBadge.tsx
@@ -1,40 +1,40 @@
 import { useQuery } from '@tanstack/react-query'
 
 export function GitHubBadge() {
-  const result = useQuery({
-    queryKey: ['repo-stats'],
-    queryFn: async ({ signal }) => {
-      const response = await fetch('https://api.github.com/repos/unional/typescript-blackbook', {
-        signal
-      })
-      return response.json()
-    }
-  })
+	const result = useQuery({
+		queryKey: ['repo-stats'],
+		queryFn: async ({ signal }) => {
+			const response = await fetch('https://api.github.com/repos/unional/typescript-blackbook', {
+				signal
+			})
+			return response.json()
+		}
+	})
 
-  if (result.data?.stargazers_count === undefined) return <></>
+	if (result.data?.stargazers_count === undefined) return <></>
 
-  return (
-    <>
-      <div className="rounded-l-sm outline outline-1 outline-gray-300 flex px-1 bg-gray-200">
-        <a
-          target="_blank"
-          aria-label="Star TypeScript Blackbook on GitHub"
-          href="https://github.com/unional/typescript-blackbook"
-          className="flex items-center"
-        >
-          <img src="/svgs/github-mark.svg" className="h-4 w-4 mr-1" alt="github" />
-          <span>Star</span>
-        </a>
-      </div>
-      <div className="rounded-r-sm outline outline-1 outline-gray-300 flex px-1 bg-gray-100">
-        <a
-          target="_blank"
-          aria-label={`${result.data?.stargazers_count} stargazers on GitHub`}
-          href="https://github.com/unional/typescript-blackbook/stargazers"
-        >
-          {result.data?.stargazers_count}
-        </a>
-      </div>
-    </>
-  )
+	return (
+		<>
+			<div className="rounded-l-sm outline outline-1 outline-gray-300 flex px-1 bg-gray-200">
+				<a
+					target="_blank"
+					aria-label="Star TypeScript Blackbook on GitHub"
+					href="https://github.com/unional/typescript-blackbook"
+					className="flex items-center"
+				>
+					<img src="/svgs/github-mark.svg" className="h-4 w-4 mr-1" alt="github" />
+					<span>Star</span>
+				</a>
+			</div>
+			<div className="rounded-r-sm outline outline-1 outline-gray-300 flex px-1 bg-gray-100">
+				<a
+					target="_blank"
+					aria-label={`${result.data?.stargazers_count} stargazers on GitHub`}
+					href="https://github.com/unional/typescript-blackbook/stargazers"
+				>
+					{result.data?.stargazers_count}
+				</a>
+			</div>
+		</>
+	)
 }
--- page/src/components/Header.tsx
+++ page/src/components/Header.tsx
@@ -4,39 +4,38 @@ import { GitHubBadge } from './GitHubBadge'
 const queryClient = new QueryClient()
 
 export function Header({ active }: { active?: 'blog' | 'blackbook' }) {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <header className="bg-violet-300">
-        <nav className="flex gap-1 p-2 items-center">
-          <div className="flex pr-4">
-            <a aria-label="home" href="/" className="hover:outline rounded-sm py-1 px-2 outline-sky-700">
-              <div className="flex font-chalk">
-                <div className="pr-1">
-                  <img src="/svgs/ts-guidelines.svg" className="w-6" alt="icon" />
-                </div>
-                Blackbook
-              </div>
-            </a>
-          </div>
-          <a
-            className={`rounded-sm hover:bg-slate-300 py-1 px-2 ${active === 'blog' ? 'underline' : ''}`}
-            href="/blogs"
-          >
-            Blogs
-          </a>
-          <a
-            className={`rounded-sm hover:bg-slate-300 py-1 px-2 ${
-              active === 'blackbook' ? 'underline' : ''
-            }`}
-            href="/blackbook"
-          >
-            Blackbook
-          </a>
-          <div className="flex py-1 flex-grow justify-end">
-            <GitHubBadge />
-          </div>
-        </nav>
-      </header>
-    </QueryClientProvider>
-  )
+	return (
+		<QueryClientProvider client={queryClient}>
+			<header className="bg-violet-300">
+				<nav className="flex gap-1 p-2 items-center">
+					<div className="flex pr-4">
+						<a aria-label="home" href="/" className="hover:outline rounded-sm py-1 px-2 outline-sky-700">
+							<div className="flex font-chalk">
+								<div className="pr-1">
+									<img src="/svgs/ts-guidelines.svg" className="w-6" alt="icon" />
+								</div>
+								Blackbook
+							</div>
+						</a>
+					</div>
+					<a
+						className={`rounded-sm hover:bg-slate-300 py-1 px-2 ${active === 'blog' ? 'underline' : ''}`}
+						href="/blogs"
+					>
+						Blogs
+					</a>
+					<a
+						className={`rounded-sm hover:bg-slate-300 py-1 px-2 ${active === 'blackbook' ? 'underline' : ''
+							}`}
+						href="/blackbook"
+					>
+						Blackbook
+					</a>
+					<div className="flex py-1 flex-grow justify-end">
+						<GitHubBadge />
+					</div>
+				</nav>
+			</header>
+		</QueryClientProvider>
+	)
 }
